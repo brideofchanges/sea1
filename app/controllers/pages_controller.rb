afb84dfe84d6feab0cac6bfceebff85a570ef280
Nikita Tselishchev <brideofchanges@gmail.com>
2016-02-05 13:47:27 +0000

    Added @variables to pages_controller, links on the home_page were updated with plan_id

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
     def home
+        @basic_plan = Plan.find(1)
+        @pro_plan = Plan.find(2)
+        @basic_company_plan = Plan.find(4)
+        @pro_company_plan = Plan.find(3)
     end
     def about 
     end
